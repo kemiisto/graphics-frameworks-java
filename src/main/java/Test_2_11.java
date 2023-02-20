618614ab13e7dec1b105f3c6b3b795818e748fb8
kemiisto <kemiisto@gmail.com>
2023-02-20 20:30:35 +0300

    feat: add Test_2_11

---
+++ src/main/java/Test_2_11.java
@@ -0,0 +1,69 @@
+import graphics.core.Attribute;
+import graphics.core.Base;
+import graphics.core.OpenGLUtils;
+import graphics.core.Uniform;
+import graphics.math.Vector;
+
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL30.*;
+
+public class Test_2_11 extends Base {
+    public int program;
+    public int vao;
+    public Uniform<Vector> translation, baseColor;
+    public float speed = 0.5f;
+
+    @Override
+    public void initialize() {
+        program = OpenGLUtils.initFromFiles("Test_2_6.vert", "Test_2_6.frag");
+
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+
+        vao = glGenVertexArrays();
+        glBindVertexArray(vao);
+
+        float[] positionData = {
+             0.0f,  0.2f,  0.0f,
+             0.2f, -0.2f,  0.0f,
+            -0.2f, -0.2f,  0.0f
+        };
+        Attribute positionAttributeTri = new Attribute("vec3", positionData);
+        positionAttributeTri.associateVariable(program, "position");
+
+        translation = new Uniform<>("vec3", new Vector(-0.5f, 0.0f, 0.0f));
+        translation.locateVariable(program, "translation");
+
+        baseColor = new Uniform<>("vec3", new Vector(1.0f, 0.0f, 0.0f) );
+        baseColor.locateVariable(program, "baseColor");
+    }
+
+    @Override
+    public void update() {
+        float distance = speed * deltaTime;
+        if (input.isKeyPressed(GLFW_KEY_LEFT)) {
+            translation.data.values[0] -= distance;
+        }
+        if (input.isKeyPressed(GLFW_KEY_RIGHT)) {
+            translation.data.values[0] += distance;
+        }
+        if (input.isKeyPressed(GLFW_KEY_DOWN)) {
+            translation.data.values[1] -= distance;
+        }
+        if (input.isKeyPressed(GLFW_KEY_UP)) {
+            translation.data.values[1] += distance;
+        }
+
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(program);
+        glBindVertexArray(vao);
+
+        translation.uploadData();
+        baseColor.uploadData();
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+    }
+
+    public static void main(String[] args) {
+        new Test_2_11().run();
+    }
+}
